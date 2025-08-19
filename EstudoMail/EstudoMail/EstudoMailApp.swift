fa2efb88c69fec5950cd0bc798d914d020e6c758
Dayô Araújo <dayoleal@10-44-48-5.0200.mack.br.iosda.org>
2025-08-19 17:28:32 -0300

    äa

---
+++ EstudoMail/EstudoMail/EstudoMailApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstudoMailApp.swift
+//  EstudoMail
+//
+//  Created by Dayô Araújo on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct EstudoMailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
