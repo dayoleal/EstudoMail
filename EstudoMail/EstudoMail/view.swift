3a3919eeae878d232a6442e4ff8b746f439fb816
hissapinto <isabelahissap@gmail.com>
2025-08-19 18:03:48 -0300

    tentando fazer window

--- EstudoMail/EstudoMail/view.swift
+++ EstudoMail/EstudoMail/view.swift
@@ -5,3 +5,17 @@
 //  Created by Dayô Araújo on 19/08/25.
 //
 
+import SwiftUI
+
+
+struct view: Scene {
+    var body: some Scene {
+        Window("What's New", id: "whats-new") {
+            Text("New in this version…")
+        }
+    }
+}
+
+//#Preview {
+//    view()
+//}
--- EstudoMail/EstudoMail/view2.swift
+++ EstudoMail/EstudoMail/view2.swift
@@ -5,3 +5,29 @@
 //  Created by Dayô Araújo on 19/08/25.
 //
 
+import SwiftUI
+
+struct view2: View {
+    @Environment(\.openWindow) var openWindow
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Button("Show What's New") {
+                        openWindow(id: "whats-new")
+                    }
+            Spacer()
+        }
+        .padding()
+        HStack {
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    view2()
+}
