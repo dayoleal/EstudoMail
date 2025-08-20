b0661e615c6302934a374e11f5426b2e90180ff9
Dayô Araújo <dayoleal@10-44-48-5.0200.mack.br.iosda.org>
2025-08-20 14:35:20 -0300

    äaa

--- EstudoMail/EstudoMail/view3.swift
+++ EstudoMail/EstudoMail/view3.swift
@@ -2,8 +2,43 @@
 import SwiftUI
 
 struct view3: View {
+    @State private var nome: String = ""
+    @State private var email: String = ""
+    @State private var senha: String = ""
+    
     var body: some View {
-        Image(systemName: "person.fill")
+        HStack {
+            Spacer()
+            VStack (alignment: .center, spacing: 20){
+                VStack (alignment: .leading, spacing: 15){
+                    Text("Nome")
+                    TextField("Nome", text: $nome)
+                        .frame(width: 200)
+                    
+                    Text("E-Mail")
+                    TextField("email@provider.com", text: $email)
+                        .frame(width: 200)
+                    
+                    Text("Senha")
+                    TextField("*******", text: $senha)
+                        .frame(width: 200)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Confirmar")
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Cancelar")
+                }
+                
+            }.padding(.vertical, 30)
+            Spacer()
+        }
     }
 }
 
